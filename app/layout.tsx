c5e50bb51d8faf01b0a9521b682448f5257b0dc8
Oliver Lulembo <omlulembo@gmail.com>
2024-12-11 10:19:35 +0200

    fonts added

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,7 @@
 import type { Metadata } from "next";
 import localFont from "next/font/local";
 import "./globals.css";
-import "../styles/sass/home_style.scss";
+import "./styles/sass/home_style.scss";
 //import "../styles/sass/style.scss"
 import { config } from '@fortawesome/fontawesome-svg-core'
 import '@fortawesome/fontawesome-svg-core/styles.css'
